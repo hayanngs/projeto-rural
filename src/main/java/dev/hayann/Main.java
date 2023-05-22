630abfffa080056a402379af9fec58a751e788c2
hayanngs <hayannow@gmail.com>
2023-05-22 00:32:46 -0300

    #1 Esqueleto do projeto

--- src/main/java/dev/hayann/Main.java
+++ src/main/java/dev/hayann/Main.java
@@ -1,7 +1,20 @@
 package dev.hayann;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
+	private final JPanel jPanel = new JPanel();
+	private final JTabbedPane tabbedPane = new JTabbedPane();
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Main app = new Main();
+		JFrame frame = new JFrame("Gerenciador Rural");
+		frame.setContentPane(app.jPanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		app.jPanel.add(app.tabbedPane);
 	}
 }
